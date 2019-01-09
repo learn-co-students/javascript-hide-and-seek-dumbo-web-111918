204fb65ed6a670f0bd22ede2338311c447df96e4
jriv95 <joel.rivera7995@gmail.com>
2019-01-09 13:03:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
 
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+// function increaseRankBy(n) {
+//   const rankedLists = document.querySelectorAll(".ranked-list")
+//   rankedLists.forEach(ulClass => {
+//     const {children} = ulClass
+//     console.log(children)
+//     for (var i = 0; i < children.length; i++) {
+//       console.log(children[i])
+//       console.log(children[i].innerText)
+//     }
+//   })
+// }
+
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
